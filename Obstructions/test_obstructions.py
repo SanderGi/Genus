93bcc60029b6edd854627d0152cf78af48cca9bf
Alexander Metzger <alexander.le@outlook.dk>
2025-07-07 15:17:36 +0200

    further test myrvold obstructions

--- Obstructions/test_obstructions.py
+++ Obstructions/test_obstructions.py
@@ -1,7 +1,23 @@
 import sys
 
+from sage.graphs.graph_generators import graphs  # type: ignore
+
 from adj_format import from_upper_tri
 from genus_algos import run_algorithm
+
+
+def has_minor(G, H):
+    """
+    Return True if H is a minor of G, False otherwise.
+    """
+    try:
+        return G.minor(H)
+    except ValueError:
+        return False
+
+
+K5 = graphs.CompleteGraph(5)
+K33 = graphs.CompleteBipartiteGraph(3, 3)
 
 
 def main(args):
@@ -13,13 +29,14 @@ def main(args):
     obstructions_path = args[0]
 
     disconnected_count = 0
+    both_planar_minors_count = 0
     with open(obstructions_path, "r") as f:
         lines = f.readlines()
         for line in lines:
             g = from_upper_tri(line)
 
             if not g.is_connected():
-                print(line)
+                print("DISCONNECTED", line)
 
                 p = g.plot()
                 p.save_image(f"disconnected{disconnected_count}.png")
@@ -29,8 +46,16 @@ def main(args):
                 genus = run_algorithm(g)
                 assert genus == 2
 
+                if has_minor(g, K33) and has_minor(g, K5):
+                    # print("BOTH", line)
+                    # p = g.plot()
+                    # p.save_image(f"both{both_planar_minors_count}.png")
+                    both_planar_minors_count += 1
+
     print("Num Disconnected", disconnected_count)
     assert disconnected_count == 3
+
+    print("Num both K5 and K3,3", both_planar_minors_count)
 
 
 if __name__ == "__main__":
